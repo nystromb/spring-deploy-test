3d0da7362e39a30e2743d4e64c1a97370fe09963
nystromb <bnystrom@8thlight.com>
2024-12-18 12:50:34 -0600

    Adds workflow

--- src/main/java/com/nystromweb/LevelController.java
+++ src/main/java/com/nystromweb/LevelController.java
@@ -29,19 +29,17 @@ public class LevelController {
                         new Level.Task(UUID.randomUUID(), "Write 2 blogs",  "",1)
                 )),
                 new Level(UUID.randomUUID(), "Green", List.of(
-                        new Level.Task(UUID.randomUUID(), "", "",1),
-                        new Level.Task(UUID.randomUUID(), "",  "",2),
-                        new Level.Task(UUID.randomUUID(), "",  "",1)
+                        new Level.Task(UUID.randomUUID(), "Build and Deploy a tic tac toe web app in a new language", "",5),
+                        new Level.Task(UUID.randomUUID(), "Implement metrics and service monitor",  "",2),
+                        new Level.Task(UUID.randomUUID(), "Write 3 blog posts",  "",1)
                 )),
                 new Level(UUID.randomUUID(), "Purple", List.of(
-                        new Level.Task(UUID.randomUUID(), "Push to git using the CLI 5 times", "",1),
-                        new Level.Task(UUID.randomUUID(), "Fix a merge conflict",  "",2),
-                        new Level.Task(UUID.randomUUID(), "",  "",1)
+                        new Level.Task(UUID.randomUUID(), "Build your own HTTP server", "",4),
+                        new Level.Task(UUID.randomUUID(), "Build an automated CI/CD pipeline",  "",2),
+                        new Level.Task(UUID.randomUUID(), "Complete 1 code kata and 3 blogs",  "",1)
                 )),
                 new Level(UUID.randomUUID(), "Blue", List.of(
-                        new Level.Task(UUID.randomUUID(), "Push to git using the CLI 5 times", "",1),
-                        new Level.Task(UUID.randomUUID(), "Fix a merge conflict",  "",2),
-                        new Level.Task(UUID.randomUUID(), "",  "",1)
+                        new Level.Task(UUID.randomUUID(), "Work on a successful client project for 4 weeks", "",5)
                 ))
         );
     }
